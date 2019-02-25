94156605236ea32ca769500ccf5037a3cdbccc41
Tobias Piechowiak <tpiechowiak@gnresound.com>
2019-02-25 16:22:46 +0100

    Changed regression analysis

--- R/Analysis.R
+++ R/Analysis.R
@@ -1,11 +1,5 @@
----
-title: "Eike Analysis"
-output: html_notebook
----
 
-First loading all packages and data...
 
-```{r}
 library('openxlsx')
 library('dplyr')
 library('ggplot2')
@@ -18,20 +12,11 @@ raw.data <- read.xlsx(paste0(data.path,'AVM_DSA_Update_EPIE_DBER.xlsx'),sheet = 
 clean.data <- raw.data[,-c(1,2,3,4,5,8,9,10,11,12,13,19,22,30,33,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,69,76,77)]
 
 
-```
-
-Originially 77 variables only 48 remain, clean for nan. Now we plot the correlation matrix 
-
-```{r, echo=TRUE}
 write.csv(clean.data, file = paste0(data.path,'Clean_data.csv'))
 
-```
 
-
-
-```{r, echo=TRUE}
 library('caret')
-new.data <- read.csv(paste0(data.path,'Cleaned_data_from_Python.csv'))
+new.data <- read.csv(paste0(data.path,'Clean_data_from_Python.csv'))
 
 data.cor <- cor(new.data,use = 'complete.obs')
 png('CorPlot.png', width = 2000, height = 2000)
@@ -40,28 +25,15 @@ dev.off()
 
 tmp <- findCorrelation(data.cor,cutoff = 0.8)
 
-filtered.data <- new.data[,-c(tmp)]
-
-data.cor <- cor(filtered.data,use = 'complete.obs')
-png('CorPlot_clean.png', width = 2000, height = 2000)
-corrplot(data.cor,method = 'circle')
-dev.off()
+filtered.data <- new.data[,-tmp]
 
 
-```
 
-
-```{r}
 fit <- lm(`Volume.of.Hemorrhage..ml.` ~ ., data = filtered.data)
 
 summary(fit)
-```
 
-```{r}
-fit.one <- lm(`Volume.of.Hemorrhage..ml.` ~ `Report..Smaller...0.no..1.yes`, data = filtered.data)
-summary(fit.one)
 
-```
 
 
 
